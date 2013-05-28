eff4a185f387b878c0d03855967f6e91434d648b
Matthewste <mat_steb@hotmail.co.uk>
2013-05-29 00:22:32 +0100

    Installer updates

---
+++ NewInstaller/OLD/UI/MainView.xaml.cs
@@ -0,0 +1,82 @@
+﻿namespace OSA.Bootstrapper
+{
+    using System.Windows;
+    using System.Windows.Controls;
+
+    /// <summary>
+    /// Description for MainView.
+    /// </summary>
+    public partial class MainView : Window
+    {      
+        Welcome welcomeView = new Welcome();
+        MySQLOptions mySqlView = new MySQLOptions();
+        InstallDirectory installDir = new InstallDirectory();
+        OSAInstallType it = new OSAInstallType();
+        Install installControl;
+
+        /// <summary>
+        /// Initializes a new instance of the MainView class.
+        /// </summary>
+        public MainView()
+        {
+            InitializeComponent();
+            installControl = new Install((MainViewModel)this.DataContext);
+            controlContent.Content = welcomeView;
+            //this.BackButton.Visibility = System.Windows.Visibility.Collapsed;
+            
+            //view.Closed += (sender, e) => BootstrapperDispatcher.InvokeShutdown();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            switch (((UserControl)controlContent.Content).Name)
+            {
+                case "WelcomeControl" :
+                    controlContent.Content = it;
+                    this.BackButton.IsEnabled = true;
+                    break;
+                case "installType":
+                    controlContent.Content = installDir;
+                    break;
+                case "installDir" :
+                    controlContent.Content = mySqlView;
+                    break;
+                case "MySqlOptionsControl":
+                    controlContent.Content = installControl;
+                    Next.IsEnabled = false;
+                    ((MainViewModel)this.DataContext).InstallEnabled = true;
+                    break;
+            }        
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            switch (((UserControl)controlContent.Content).Name)
+            {
+                case "WelcomeControl":
+                    controlContent.Content = it;
+                    break;
+                case "installType":
+                    this.BackButton.IsEnabled = false;
+                    controlContent.Content = welcomeView;
+                    break;
+                case "installDir":
+                    controlContent.Content = it;
+                    break;
+                case "MySqlOptionsControl":
+                    controlContent.Content = installDir;                   
+                    break;
+                case "installControl":
+                    controlContent.Content = mySqlView;
+                    ((MainViewModel)this.DataContext).InstallEnabled = false;
+                    Next.IsEnabled = true;
+                    break;
+            }  
+        }  
+    }
+}
---
+++ NewInstaller/OLD/UI/OSA BA.cs
@@ -0,0 +1,45 @@
+﻿namespace OSA.Bootstrapper
+{
+    using Microsoft.Tools.WindowsInstallerXml.Bootstrapper;
+    using System;
+    using System.Windows;
+    using System.Windows.Threading;
+
+    public class OSABA : BootstrapperApplication
+    {
+
+        // global dispatcher
+        static public Dispatcher BootstrapperDispatcher { get; private set; }
+
+        // entry point for our custom UI
+        protected override void Run()
+        {
+            try
+            {
+                this.Engine.Log(LogLevel.Verbose, "Launching OSA BA UX");
+                BootstrapperDispatcher = Dispatcher.CurrentDispatcher;
+
+                //Form1 f = new Form1(this);
+                //f.ShowDialog();
+                MainViewModel viewModel = new MainViewModel(this);
+                this.Engine.Detect();
+
+                MainView view = new MainView();
+
+                view.DataContext = viewModel;
+                view.Closed += (sender, e) => BootstrapperDispatcher.InvokeShutdown();
+                view.Show();
+
+                Dispatcher.Run();
+            }
+            catch (Exception ex)
+            {
+                this.Engine.Log(LogLevel.Verbose, "Exception Caught by BA details:" + ex.Message);
+                // change to use engine log
+                MessageBox.Show("Exception" + ex.Message);
+            }
+
+            this.Engine.Quit(0);
+        }
+    }
+}
