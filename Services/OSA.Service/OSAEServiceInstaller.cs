8b0662765e203358d26cff47e36b8a77a232f1c7
Matthewste <mat_steb@hotmail.co.uk>
2013-05-29 23:07:34 +0100

    Repo restructure

---
+++ Services/OSA.Service/OSAEServiceInstaller.cs
@@ -0,0 +1,36 @@
+﻿namespace OSAEService
+{
+    using System.ComponentModel;
+    using System.Configuration.Install;
+    using System.ServiceProcess;
+
+    [RunInstaller(true)]
+    public class OSAEServiceInstaller : Installer
+    {
+        /// <summary>
+        /// Public Constructor for WindowsServiceInstaller.
+        /// - Put all of your Initialization code here.
+        /// </summary>
+        public OSAEServiceInstaller()
+        {
+            ServiceProcessInstaller serviceProcessInstaller = new ServiceProcessInstaller();
+            ServiceInstaller serviceInstaller = new ServiceInstaller();
+
+            //# Service Account Information
+            serviceProcessInstaller.Account = ServiceAccount.NetworkService;
+            serviceProcessInstaller.Username = null;
+            serviceProcessInstaller.Password = null;
+
+            //# Service Information
+            serviceInstaller.DisplayName = "OSAE";
+            serviceInstaller.StartType = ServiceStartMode.Automatic;            
+
+            // This must be identical to the WindowsService.ServiceBase name
+            // set in the constructor of WindowsService.cs
+            serviceInstaller.ServiceName = "OSAE";
+
+            this.Installers.Add(serviceProcessInstaller);
+            this.Installers.Add(serviceInstaller);
+        }
+    }
+}
