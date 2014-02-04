c110da2af819912f2ac14dae0c65793dfde1a14c
Automate1 <jr@flagg.us>
2014-02-03 20:13:15 -0500

    Updated OSA Manager application name

--- UI/Manager/Properties/AssemblyInfo.cs
+++ UI/Manager/Properties/AssemblyInfo.cs
@@ -7,12 +7,12 @@ using System.Windows;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Manager_WPF")]
+[assembly: AssemblyTitle("OSA Manager")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Manager_WPF")]
-[assembly: AssemblyCopyright("Copyright ©  2012")]
+[assembly: AssemblyProduct("OSA Manager")]
+[assembly: AssemblyCopyright("Copyright ©  2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
