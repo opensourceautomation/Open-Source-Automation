f3f3d8f6fcd31ba27837699d1105579676db2b18
VaughnRupp <vaughnrupp@yahoo.com>
2015-12-07 13:27:20 -0600

    Eventghost plugin added

---
+++ Plugins/EventGhost/EventGhost.cs
@@ -0,0 +1,194 @@
+﻿namespace OSAE.EventGhost
+{
+    using System;
+    using System.Text;
+    using System.Net;
+    using System.Net.Sockets;
+
+    public class EventGhost : OSAEPluginBase
+    {
+        /// <summary>
+        /// Provides access to logging
+        /// </summary>
+       // Logging logging = Logging.GetLogger("EventGhost");
+        private static OSAE.General.OSAELog Log = new General.OSAELog();
+
+        /// <summary>
+        /// Plugin name
+        /// </summary>
+        string pName;
+        
+        
+
+        /// <summary>
+        /// OSA Plugin Interface - Commands the be processed by the plugin
+        /// </summary>
+        /// <param name="method">Method containging the command to run</param>
+        public override void ProcessCommand(OSAEMethod method)
+        {
+
+            
+            String methodName = method.MethodName;
+            String objectName = method.ObjectName;
+            String combinedString = objectName + "_" + methodName;
+           
+            //get the called objects properties to see if there is a computer objet defined 
+            OSAEObject calledObject = OSAEObjectManager.GetObjectByName(method.ObjectName);
+            OSAEObjectPropertyCollection ComputerProperty = calledObject.Properties;
+            OSAEObjectCollection eventGhostObjects = OSAEObjectManager.GetObjectsByType("Computer");
+            OSAEObject computer = new OSAEObject();
+            
+            
+            
+            //if there are no properties defined then grab the first available Computer object     
+            //logging.AddToLog("The Count of the Computer Properties is : " + ComputerProperty.Count, true);
+            if (ComputerProperty.Count == 0)
+            {
+                         
+                //get the last object in the list
+                
+                foreach (OSAEObject test in eventGhostObjects)
+                {
+                    computer = test;
+                }
+
+                //add log entry if mulptiple computer objects were dectected and no property was used to select which object
+                if (eventGhostObjects.Count > 1) {
+
+                                      
+                    Log.Info("There are multiple eventghost computer objects detected.  " + computer.Name + " with an address of: " + OSAEObjectPropertyManager.GetObjectPropertyValue(computer.Name, "IPAddress") + ":" + OSAEObjectPropertyManager.GetObjectPropertyValue(computer.Name, "EventGhost Port") + " is being used to transmit the package.  Please add a property called 'Computer' with a value of the object name of the computer you wish to transmit the UDP packet to.");
+                }
+            
+            
+            }
+                // if the object type contains a property with ID "Computer" use that property to select the appropriate object
+            else
+            {
+
+                OSAEObjectProperty whichComputer = OSAEObjectPropertyManager.GetObjectPropertyValue(calledObject.Name,"Computer");
+                computer = eventGhostObjects.Find(whichComputer.Value);
+
+                //check to see if a computer object was found if none print the logged property value and select another object to use
+                if (computer == null){
+                    foreach (OSAEObject test in eventGhostObjects)
+                    {
+                        computer = test;
+                    }
+
+                    if (eventGhostObjects.Count > 1)
+                    {
+                        Log.Info("A computer object was unable to be matched to " + whichComputer.Value + " please ensure that " + calledObject.Name + " contains a property called Computer with a value mathing the name of the appropriate computer object.");
+                        Log.Info(computer.Name + " with an address of: " + computer.Address + ":" + OSAEObjectPropertyManager.GetObjectPropertyValue(computer.Name, "EventGhost Port") + " is being used to transmit the package.");
+                    }
+                }
+
+            }
+
+
+
+         //   logging.AddToLog(address, true);
+          //  logging.AddToLog(objectName, true);
+           // logging.AddToLog(combinedString, true);
+           
+
+            //get the server address indicated in the address for the object passed to the method
+            String ipAddress = computer.Address;
+
+            //check to make sure computer object has address, if not assume local host
+            if (ipAddress.Equals("", StringComparison.Ordinal)) {
+                ipAddress = "Localhost";
+            
+            }
+
+            OSAEObjectProperty portAddress = OSAEObjectPropertyManager.GetObjectPropertyValue(computer.Name, "Port");
+            
+            //if not value for port use default value of 33333
+            int port = 0;
+
+            if ((portAddress.Value).Equals("",StringComparison.Ordinal)){
+             port = 33333;
+            }  
+            else{
+            port = Int32.Parse(portAddress.Value);
+            }
+            
+           
+
+           // logging.AddToLog(port, true);
+          // logging.AddToLog(ipAddress, true);
+            IPAddress serverAddr = IPAddress.Parse(ipAddress);
+            IPEndPoint endPoint = new IPEndPoint(serverAddr, port);
+          
+            //send the updpacket
+            UdpClient udp = new UdpClient();
+            byte[] sendBytes = Encoding.ASCII.GetBytes(combinedString); 
+            udp.Send(sendBytes, sendBytes.Length, endPoint);          
+
+
+          
+        }
+
+        /// <summary>
+        /// OSA Plugin Interface - called on start up to allow plugin to do any tasks it needs
+        /// </summary>
+        /// <param name="pluginName">The name of the plugin from the system</param>
+        public override void RunInterface(string pluginName)
+        {
+            pName = pluginName;
+
+            try
+            {
+                Log.Info("Starting EventGhost...");
+
+                // on startup ensure that there is at least one computer setup to receive eventghost commands
+                OSAEObjectCollection computers = OSAEObjectManager.GetObjectsByType("COMPUTER");
+                
+                // check to make sure that the Compuer Objecttype has the needed property
+                //add eventghost property to the computer object type
+               
+                // need to implement check before i use the method below
+                
+                // OSAEObjectTypeManager.ObjectTypePropertyAdd("EventGhost Port", "String", "", "Computer", false);
+               
+
+                
+                
+                foreach (OSAEObject acomputer in computers){
+                
+                    String computerName = acomputer.Name;
+                    String IPAddress = acomputer.Address;
+                    String eventGhostPort = (OSAEObjectPropertyManager.GetObjectPropertyValue(computerName, "EventGhost Port").Value);
+
+                    //if no port for property indicate that the default port of 333333 will be used
+                    if (eventGhostPort.Equals("", StringComparison.Ordinal)) {
+                        Log.Info("There is no port information for " + acomputer.Name + " the default port of 33333 will be used when this computer is called");
+                    }
+                    //check to see if computer has address, if no log that "local host will be used"
+                    if ((acomputer.Address).Equals("", StringComparison.Ordinal)){
+                    Log.Info("There is no address information for " + acomputer.Name + "localhost will be used for this location");
+                    }
+
+                }
+
+            }
+            catch (Exception ex)
+            {
+                Log.Error("Error during RunInterface: " + ex.Message);
+            }
+ 
+        
+        
+        
+        }
+
+        /// <summary>
+        /// OSA Plugin Interface - The plugin has been asked to shut down
+        /// </summary>        
+        public override void Shutdown()
+        {
+            Log.Info("Stopping EventGhost...");
+
+            // Place any code required to shut down your plugin here leave empty if no action is required
+        }
+    }
+}
---
+++ Plugins/EventGhost/install.sql
@@ -0,0 +1,2 @@
+CALL osae_sp_object_type_add ('EventGhost','EventGhost','','Plugin',1,0,0,1);
+CALL osae_sp_object_type_property_add ('Computer','EventGhost Port','String','','',0);
