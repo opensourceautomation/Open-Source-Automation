64f42896ac9c82dbb0f701e02bcd5313a75ed461
KHerron <k.herron>
2016-04-17 21:01:14 -0400

    Added Mouse Up/Down Events to WebUI Click Control

--- UI/Web/controls/ctrlClickImage.ascx.cs
+++ UI/Web/controls/ctrlClickImage.ascx.cs
@@ -1,33 +1,48 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
 using OSAE;
 
 public partial class controls_ctrlClickImage : System.Web.UI.UserControl
 {
     public OSAEObject screenObject { get; set; }
-    public string ObjectName;
-    public string MethodName;
-    public string ScriptName;
-    public string mp1 = "", mp2 = "", sp1 = "", sp2 = "";
+    public string pressObjectName;
+    public string releaseObjectName;
+    public string pressMethodName;
+    public string releaseMethodName;
+    public string pMp1;
+    public string pMp2;
+    public string rMp1;
+    public string rMp2;
     private OSAEImageManager imgMgr = new OSAEImageManager();
 
     protected void Page_Load(object sender, EventArgs e)
     {
-        ObjectName = screenObject.Property("Press Object Name").Value;
-        MethodName = screenObject.Property("Press Method Name").Value;
-        mp1 = screenObject.Property("Press Method Param 1").Value;
-        mp2 = screenObject.Property("Press Method Param 2").Value;
-        ScriptName = screenObject.Property("Press Script Name").Value;
-        sp1 = screenObject.Property("Press Script Param 1").Value;
-        sp2 = screenObject.Property("Press Script Param 2").Value;
-
-        string imgName = screenObject.Property("Pressed Image").Value;
-        OSAEImage img = imgMgr.GetImage(imgName);
-        if (img != null)
-        {
-            imgClickImage.ImageUrl = "~/ImageHandler.ashx?id=" + img.ID;
-            imgClickImage.Attributes.Add("Style", "position:absolute;top:" + (Int32.Parse(screenObject.Property("Y").Value) + 50).ToString() + "px;left:" + (Int32.Parse(screenObject.Property("X").Value) + 10).ToString() + "px;z-index:" + (Int32.Parse(screenObject.Property("ZOrder").Value) + 10).ToString() + ";");
-            imgClickImage.ToolTip = ObjectName + " - " + MethodName;
-            imgClickImage.Attributes.Add("onclick", "runMethod('" + ObjectName + "','" + MethodName + "','" + mp1 + "','" + mp2 + "');");
-        }
+        pressObjectName = screenObject.Property("Press Object Name").Value;
+        releaseObjectName = screenObject.Property("Press Object Name").Value;
+        OSAEObject pObj = OSAEObjectManager.GetObjectByName(pressObjectName);
+        OSAEObject rObj = OSAEObjectManager.GetObjectByName(releaseObjectName);
+        string pressObjTrust = pObj.MinTrustLevel.ToString();
+        string releaseObjTrust = rObj.MinTrustLevel.ToString();
+        pressMethodName = screenObject.Property("Press Method Name").Value;
+        releaseMethodName = screenObject.Property("Release Method Name").Value;
+        pMp1 = screenObject.Property("Press Method Param 1").Value;
+        pMp2 = screenObject.Property("Press Method Param 2").Value;
+        rMp1 = screenObject.Property("Release Method Param 1").Value;
+        rMp2 = screenObject.Property("Release Method Param 2").Value;
+        string normalImgName = screenObject.Property("Normal Image").Value;
+        string pressImgName = screenObject.Property("Pressed Image").Value;
+        OSAEImage img = imgMgr.GetImage(normalImgName);
+        OSAEImage img2 = imgMgr.GetImage(pressImgName);
+        string normalImgURL = "ImgHandler.aspx?ImageID=" + img.ID;
+        string pressImgURL = "ImgHandler.aspx?ImageID=" + img2.ID;
+        imgClickImage.ImageUrl = "~/imgHandler.aspx?ImageID=" + img.ID;
+        imgClickImage.Attributes.Add("Style", "position:absolute;top:" + (Int32.Parse(screenObject.Property("Y").Value) + 50).ToString() + "px;left:" + (Int32.Parse(screenObject.Property("X").Value) + 10).ToString() + "px;z-index:" + (Int32.Parse(screenObject.Property("ZOrder").Value) + 10).ToString() + ";");
+        imgClickImage.ToolTip = pressObjectName + " - " + pressMethodName;
+        imgClickImage.Attributes.Add("onmousedown", "methFunc('" + pressObjectName + "','" + pressMethodName + "','" + pMp1 + "','" + pMp2 + "','" + pressObjTrust + "','" + pressImgURL + "', this.id);");
+        imgClickImage.Attributes.Add("onmouseup", "methFunc('" + releaseObjectName + "','" + releaseMethodName + "','" + rMp1 + "','" + rMp2 + "','" + releaseObjTrust + "','" + normalImgURL + "', this.id);");
     }
 }
