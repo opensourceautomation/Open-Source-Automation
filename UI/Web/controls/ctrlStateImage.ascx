c975dddf6607748dfedce36526e32f832025d687
KHerron <k.herron>
2016-04-17 21:00:48 -0400

    Applied Trust Level & Gif support

--- UI/Web/controls/ctrlStateImage.ascx
+++ UI/Web/controls/ctrlStateImage.ascx
@@ -1,5 +1,15 @@
 ﻿<%@ Control Language="C#" AutoEventWireup="true" CodeFile="ctrlStateImage.ascx.cs" Inherits="controls_ctrlStateImage" className="ctrlStateImage"%>
+<script type="text/javascript">
+    function methFunc(object, method, p1, p2, objTrust, img, n)
+    {
+        var sI = document.getElementById(n);
+        sI.setAttribute('src', img);
+        runMethod(object, method, p1, p2, objTrust);
+    }
+    
+</script>
 
 <asp:Image ID="imgStateImage" runat="server"/>
 <asp:HiddenField ID="hdnCurState" runat="server" />
 <asp:HiddenField ID="hdnObjName" runat="server" />
+
--- UI/Web/controls/ctrlStateImage.ascx.cs
+++ UI/Web/controls/ctrlStateImage.ascx.cs
@@ -1,9 +1,19 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.IO;
+using System.Drawing;
+using System.Web.UI.WebControls;
+
+
 using OSAE;
 
 public partial class controls_ctrlStateImage : System.Web.UI.UserControl
 {
     public OSAEObject screenObject { get; set; }
+    public OSAEObject cObj;
     public DateTime LastUpdated;
     public DateTime LastStateChange;
     public string ObjectName;
@@ -17,9 +27,10 @@ public partial class controls_ctrlStateImage : System.Web.UI.UserControl
         ObjectName = screenObject.Property("Object Name").Value;
         hdnObjName.Value = ObjectName;
         CurState = OSAEObjectStateManager.GetObjectStateValue(ObjectName).Value;
+        cObj = OSAEObjectManager.GetObjectByName(ObjectName);
         hdnCurState.Value = CurState;
         LastStateChange = OSAEObjectStateManager.GetObjectStateValue(ObjectName).LastStateChange;
-       
+
         foreach (OSAEObjectProperty p in screenObject.Properties)
         {
             if (p.Value.ToLower() == CurState.ToLower()) StateMatch = p.Name.Substring(0, p.Name.LastIndexOf(' '));
@@ -30,17 +41,26 @@ public partial class controls_ctrlStateImage : System.Web.UI.UserControl
 
         string imgName = screenObject.Property(StateMatch + " Image").Value;
         OSAEImage img = imgMgr.GetImage(imgName);
+        bool x = File.Exists(OSAE.Common.ApiPath +  "/Plugins/Web Server/wwwroot/Images/" + img.Name + ".gif");
         if (img != null)
         {
-            imgStateImage.ImageUrl = "~/ImageHandler.ashx?id=" + img.ID;
+            if (x == false)
+            {
+                imgStateImage.ImageUrl = "~/ImageHandler.ashx?id=" + img.ID;
+            }
+            else
+            {
+                imgStateImage.ImageUrl = "~/Images/" + imgName + ".gif";
+            }
             imgStateImage.Attributes.Add("Style", "position:absolute;top:" + (Int32.Parse(screenObject.Property(StateMatch + " Y").Value) + 50).ToString() + "px;left:" + (Int32.Parse(screenObject.Property(StateMatch + " X").Value) + 10).ToString() + "px;z-index:" + (Int32.Parse(screenObject.Property("ZOrder").Value) + 10).ToString() + ";opacity:" + LightLevel / 100.00 + ";");
+            imgStateImage.Attributes.Add("Alt", "~/Images/" + imgName + ".gif");
             imgStateImage.ToolTip = ObjectName + "\n" + CurState + " since: " + LastStateChange;
 
             //REPLACE THIS with code block in screens to use state match and not on/off!
             if (CurState == "ON")
-                imgStateImage.Attributes.Add("onclick", "runMethod('" + ObjectName + "','OFF','','');");
+                imgStateImage.Attributes.Add("onclick", "runMethod('" + ObjectName + "','OFF','','', " + cObj.MinTrustLevel + ");");
             else
-                imgStateImage.Attributes.Add("onclick", "runMethod('" + ObjectName + "','ON','','');");
+                imgStateImage.Attributes.Add("onclick", "runMethod('" + ObjectName + "','ON','','', " + cObj.MinTrustLevel + ");");
         }
     }
 
